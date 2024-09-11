d3106e802950b9a738872076f108770cd9cc8eb6
Fábio Pinto Figueiredo <pf95fabio@gmail.com>
2024-09-11 09:20:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-
 const url = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos";
 
 
+function listacidades(){
+    let cidades = document.getElementById("cidades")
+    document.getElementById("cidades").replaceChildren()
+    cidades.innerHTML = "agora selecione uma cidade: "
+   
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    data.forEach(cidade => {
-        if(cidade["municipio"]["microrregiao"]["mesorregiao"]["UF"]["nome"] == "Rio de Janeiro"){
-            const cidadecontainer = document.createElement("div")
-            const title = document.createElement("p");
-            title.textContent = `nome da cidade: ${cidade["nome"]}`;
-            cidadecontainer.appendChild(title);
-            document.getElementById("cidades").appendChild(cidadecontainer);
-        }
-        console.log(cidade["municipio"]["microrregiao"]["mesorregiao"]["UF"]["nome"])
+        const cidadecontainer = document.createElement("div")
+        const select = document.createElement("select");
+        data.forEach(cidade => {
+            if(cidade["municipio"]["microrregiao"]["mesorregiao"]["UF"]["sigla"] == document.getElementById("estados").value){
+                const option = document.createElement("option");
+                option.innerHTML = `${cidade["nome"]}`;
+                cidadecontainer.appendChild(select);
+                select.appendChild(option);
+                cidades.appendChild(cidadecontainer);
+            }
+        })
     })
-})
-.catch(error=>console.error("Erro no servidor:", error))
+    .catch(error=>console.error("Erro no servidor:", error))
+}
